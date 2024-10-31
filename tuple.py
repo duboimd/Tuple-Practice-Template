28872f27243994606e49a4d51e4fc69ba74bf323
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-10-31 10:32:17 -0600

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,18 @@
+print("Question 1:")
+#Q1: Create a tuple with single item 50 and print your tuple
+
+print()
+print("Question 2:")
+#Q2: Access the value 4 and print it from tuple1
+tuple1 = ("Purple", "Pink", 17, [1, 2, 3, 4, 5], (5, 15, 25))
+
+
+print()
+print("Question 3:")
+#Q3: Create a new tuple with 6 foods called foods.
+# a. Print the tuple
+# b. Print the length
+# c. Print the first item in your tuple
+# d. Print the last item in your tuple
+
+
